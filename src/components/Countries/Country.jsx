17bc188fc73e664d21efbdc631f1e7607d6a17cd
Shahriar Hasan Siam <siamshahriarhasan@gmail.com>
2025-05-24 22:59:42 +0600

    added Visited button, changing the button value according to the current state

--- src/components/Countries/Country.jsx
+++ src/components/Countries/Country.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./Country.css";
 
 const Country = ({ country }) => {
-  console.log(country);
+  const [visited, setVisited] = useState(false);
   const { name, flags } = country;
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+
+  // console.log(country);
   return (
     <div className="country">
       <div>
         <p>
           Name: <b style={{ fontSize: "20px" }}>{name.common}</b>
         </p>
+        <button onClick={handleVisited}>{visited ? "Visited" : "Visit Now"}</button>
       </div>
       <div>
         <img src={flags?.png} alt={flags?.alt} />
